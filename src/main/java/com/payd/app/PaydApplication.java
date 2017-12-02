1dbc87fe9a1fec85d9810d214ff380cddfb4539e
kxe2392 <kd.lft13@gmail.com>
2017-12-01 19:59:57 -0600

    Inital setup

---
+++ src/main/java/com/payd/app/PaydApplication.java
@@ -0,0 +1,12 @@
+package com.payd.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydApplication.class, args);
+	}
+}
